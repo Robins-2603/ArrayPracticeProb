67320ed94bef5f785da2c1cb8d9727dc434fc12b
Robins-2603 <robinsforwork@gmail.com>
2022-08-09 23:49:07 +0530

    rename this file

--- primeFactor.js
+++ primeFactor.js
@@ -4,8 +4,8 @@
 
 let primeFactorsArray = new Array();
 
-const readline = require("readline-sync");
-let n = parseInt(readline.question("Enter the number: "))
+const prompt = require("prompt-sync")();
+let n = parseInt(prompt("Enter the number: "))
 console.log(`The Prime Factors for ${n} are: `);
 for(i =2; i<=n; i++){
     while(n%i == 0){
